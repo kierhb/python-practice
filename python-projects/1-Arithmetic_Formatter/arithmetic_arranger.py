3e6ddb7a4dd1b16f9a803ecf71f1b2adb1f2018d
kierhb <kierbobila@gmail.com>
2022-08-25 16:11:54 +0800

    Add code for first row

--- python-projects/1-Arithmetic_Formatter/arithmetic_arranger.py
+++ python-projects/1-Arithmetic_Formatter/arithmetic_arranger.py
@@ -1,38 +1,3 @@
-def ar_ar(a):
-    #Trial
-    if len(a) >= 5:
-        set1 = a.split()
-        #First Number
-        a1 = set1[0]
-        if isinstance(int(set1[0]), int) == True:
-            a1 = int(set1[0])
-
-        #Second Number
-        b1 = set1[2]
-        if isinstance(int(set1[2]), int) == True:
-            b1 = int(set1[2])
-
-        #Operation
-        c1 = set1[1]
-        if c1 == '+':
-            opr = a1 + b1
-        elif c1 == '-':
-            opr = a1 - b1
-        else:
-            opr = 'abort'
-    else:
-        print('Error: Too many problems')
-
-    print('  ', a1)
-    print('+ ', b1)
-    print('----')
-
-    if len(a1) == 
-
-    print(opr)
-
-ar_ar('235 - 52')
-
 def arithmetic_arranger(problems, solver = False):
     #Checking for problems
     if len(problems) > 5:
@@ -43,7 +8,7 @@ def arithmetic_arranger(problems, solver = False):
     operator = []
 
     for problem in problems:
-        parts = problem.split[]
+        parts = problem.split()
         first_operand.append(parts[0])
         second_operand.append(parts[2])
         operator.append(parts[1])
@@ -62,8 +27,18 @@ def arithmetic_arranger(problems, solver = False):
         if len(first_operand) > 4 < len(second_operand):
             return "Error: Numbers cannot be more than four digits"
     
-    
+    #Start of Operation
+    first_row = []
+    second_row = []
+    third_row = []
+    fourth_row = []
+
+    # For First Row
+    for i in range(len(first_operand)):
+        if len(first_operand[i]) > len(second_operand[i]):
+            first_row.append(" " + first_operand[i])
+        else:
+            first_row.append(" " * (len(second_operand[i]) - len(first_operand[i]) + 2) + first_operand[i])
 
 
-
-    
+arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"])
