616625a6d6bbec5082ebf94358f4ed3a6426bc09
kierhb <kierbobila@gmail.com>
2022-09-21 15:26:00 +0800

    Practice Recursion

---
+++ other/recursion.py
@@ -0,0 +1,12 @@
+# Recursion
+# common use is by factorial calculation
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n-1)
+
+
+print(factorial(3))
+print(factorial(4))
+print(factorial(5))
