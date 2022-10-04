6ef397d874ccde1d2e1c990e9fee1612bc27c113
kierhb <kierbobila@gmail.com>
2022-10-04 13:07:05 +0800

    Add checking for operations

--- python-projects/1-Arithmetic_Formatter/main.py
+++ python-projects/1-Arithmetic_Formatter/main.py
@@ -8,5 +8,27 @@ def arithmetic_arranger(problems, solver = False):
         '''
         return "Error: Too many problems"
 
-    
+
+    first_operand = []
+    second_operand = []
+    operators = []
+
+
+    for problem in problems:
+        if match := re.search(r"([0-9]+)\s([+, -])\s([0-9]+)", problem):
+            # Checking Operation
+            '''
+                The appropriate operators the function will accept are addition and subtraction. Multiplication and division will return an error. Other operators not mentioned in this bullet point will not need to be tested. The error returned will be: Error: Operator must be '+' or '-'.
+            '''
+            operators.append(match.group(2))
+            if '*' in operators or '/' in operators:
+                return "Error: Operator must be '+' or '-'"
+
+
+
+
+    return operators
+
+
+
 print(arithmetic_arranger(["2 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
