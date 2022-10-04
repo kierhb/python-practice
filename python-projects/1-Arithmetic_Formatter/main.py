ca8ef1c651bdafb8649a41ced3ac59b660e0d3ff
kierhb <kierbobila@gmail.com>
2022-10-04 13:36:41 +0800

    Add checking for digits

--- python-projects/1-Arithmetic_Formatter/main.py
+++ python-projects/1-Arithmetic_Formatter/main.py
@@ -15,19 +15,35 @@ def arithmetic_arranger(problems, solver = False):
 
 
     for problem in problems:
-        if match := re.search(r"([0-9]+)\s([+, -])\s([0-9]+)", problem):
+        if match := re.search(r"([a-z0-9]+)\s([+, -])\s([a-z0-9]+)", problem):
+        
+            first_operand.append(match.group(1))
+            second_operand.append(match.group(3))
+            operators.append(match.group(2))
+
+
             # Checking Operation
             '''
                 The appropriate operators the function will accept are addition and subtraction. Multiplication and division will return an error. Other operators not mentioned in this bullet point will not need to be tested. The error returned will be: Error: Operator must be '+' or '-'.
             '''
-            operators.append(match.group(2))
             if '*' in operators or '/' in operators:
                 return "Error: Operator must be '+' or '-'"
 
 
+            # Checking Digits
+            '''
+                Each number (operand) should only contain digits. Otherwise, the function will return: Error: Numbers must only contain digits.
+            '''
+            for i in range(len(first_operand)):
+                if not (first_operand[i].isdigit() and second_operand[i].isdigit()):
+                    return "Error: Numbers must only contain digits"
+            
 
 
-    return operators
+    print(first_operand)
+    #print(operators)
+    #print(second_operand)
+
 
 
 
